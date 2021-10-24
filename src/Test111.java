eb599d372b086b783f697b8263220e99804d4799
zs <zs@bjpowernode.com>
2021-10-24 12:52:08 +0800

    log 1

---
+++ src/Test111.java
@@ -0,0 +1,17 @@
+/**
+ * ClassName:T
+ * Package:PACKAGE_NAME
+ * Description:
+ *
+ *
+ * @author XM
+ * @date 2021/9/3 14:34
+ */
+public class Test111 {
+    public static void main(String[] args) {
+        Integer integer = 123;
+
+    }
+
+
+}
---
+++ src/test11.java
@@ -0,0 +1,6 @@
+public class test11 {
+    public static void main(String[] args) {
+        Object o = new Object();
+        Object o1 = new Object();
+    }
+}
